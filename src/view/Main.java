d512bc10b45801a82111bc11b35d44c8be8c7393
Leonardo Ferreira <74208182+lilzji@users.noreply.github.com>
2024-03-02 18:26:52 -0300

    Primeiro Commit

---
+++ src/view/Main.java
@@ -0,0 +1,31 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.KillController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		KillController killController = new KillController(); 
+		int opc = Integer.parseInt(JOptionPane.showInputDialog("Digite o processo que voce deseja\n1 - Listar Processos\n2 - Matar por PID\n3 - Matar por Nome\n9 - Finalizar"));
+		switch(opc) {
+			case 1: 
+				killController.listaProcessos();
+				break;
+			case 2:
+				int pid = Integer.parseInt(JOptionPane.showInputDialog("Digite o PID: "));
+				killController.mataPid(pid);
+				break;
+			case 3:
+				String nome = JOptionPane.showInputDialog("Digite o nome: ");
+				killController.mataNome(nome);
+				break;
+			case 9:
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "Opcao Invalida");
+		}
+	}
+
+}
